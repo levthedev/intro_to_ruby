aa47f4db84f887326193b2d8460eec4995abc4ac
Lev Kravinsky <kravinskylev@gmail.com>
2016-11-13 16:24:23 -0500

    hash

---
+++ hash.rb
@@ -0,0 +1,10 @@
+#hash: associates keys (symbols) to value(s); here are arrays for values
+food = {
+  treats: ["jellies", "pbs", "cake"],
+  ravs: ["truffle", "artichoke", "butternut squash"],
+  sauce: ["peso", "alfredo", "tomat"]
+}
+
+print food
+print food[:treats]
+print food["treats"]
