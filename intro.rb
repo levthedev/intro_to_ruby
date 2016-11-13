e471d12a693fd86ec87a74afd683553412967139
Lev Kravinsky <kravinskylev@gmail.com>
2016-11-13 16:24:14 -0500

    intro

---
+++ intro.rb
@@ -0,0 +1,17 @@
+# 1, 2, 3, 500 are integers
+# "h", "hi", 'yo' is a string
+# this is
+# a
+# comment
+# arrays
+# print and puts both output, but print outputs as is and puts adds lines
+my_array = ["yo", "I", "am", "an", "array"]
+
+# conditional, control flow
+if false
+  print my_array[-3..-1]
+elsif false
+  print("c")
+else
+  print("always")
+end
